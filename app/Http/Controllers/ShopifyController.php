2980d6153354177fbe3c4201147ec57990380c5f
Hadi Hassan <hadihassan09@gmail.com>
2021-08-20 22:01:11 +0300

    created authentication

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -2,14 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-class ShopifyController extends Controller
+class ShopifyController extends BaseController
 {
     public function index(){
-        $url = 'https://bcc4677346426e146a6e0c63d4730dc5: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@razanes-store.myshopify.com/admin/api/2021-07/products.json';
-        $client =  new \GuzzleHttp\Client(['verify' => false ]    );
-        $response = $client->request('GET', $url);
-        return $response->getBody();
+        return $this->performRequest('/2021-07/products.json');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ShopifyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,4 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/shopify', [ShopifyController::class,'index']);
+// routes/api.php
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/shopify', [ShopifyController::class,'index']);
+});
